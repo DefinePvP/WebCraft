c07dd3fb670149b4e90ac67d25a0fe2913e131e8
DefinePvP <hmontey@outlook.com>
2019-03-06 17:14:09 -0600

    Render.js has been broken.
    I've broken it so it works on school chromebooks.

--- js/render.js
+++ js/render.js
@@ -106,7 +106,8 @@ function Renderer( id )
 	{
 		gl.bindTexture( gl.TEXTURE_2D, terrainTexture );
 		gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, terrainTexture.image );
-		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST );
 	};
 	terrainTexture.image.src = "media/terrain.png";
